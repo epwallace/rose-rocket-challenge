74ec00e3ca4077cd5242b4d924fa91427b9f714a
Evan Wallace <evanpatrickwallace@gmail.com>
2021-01-23 09:51:54 -0500

    improve coordinate specificity in initial state

--- client/src/slices/movementsSlice.js
+++ client/src/slices/movementsSlice.js
@@ -7,20 +7,20 @@ const movementsSlice = createSlice({
   initialState: [
     {
       id: 11110001,
-      origin: { lat: 43.6511, lng: -79.347 },
-      destination: { lat: 45.4215, lng: -75.6972 },
+      origin: { lat: 43.6534817, lng: -79.3839347 },
+      destination: { lat: 45.421106, lng: -75.690308 },
       description: "Toronto -> Ottawa",
     },
     {
       id: 11110002,
-      origin: { lat: 45.4215, lng: -75.6972 },
-      destination: { lat: 45.5017, lng: -73.5673 },
+      origin: { lat: 45.421106, lng: -75.690308 },
+      destination: { lat: 45.4972159, lng: -73.6103642 },
       description: "Ottawa -> Montreal",
     },
     {
       id: 11110003,
-      origin: { lat: 46.4917, lng: -80.993 },
-      destination: { lat: 45.4215, lng: -75.6972 },
+      origin: { lat: 46.487489, lng: -80.9921545 },
+      destination: { lat: 45.421106, lng: -75.690308 },
       description: "Sudbury -> Ottawa",
     },
   ],
